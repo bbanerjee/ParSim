5306f2b9ecbd41f50ed6b8174a5881454c89ad04
bbanerjee <b.banerjee.nz@gmail.com>
2013-01-21 15:07:10 +1300

    A few more format and warning fixes.

--- EMUNE/src/module_precision.f90
+++ EMUNE/src/module_precision.f90
@@ -1,11 +1,11 @@
-  module precision
-! define the precision to be used, and other constant values
+module precision
+  ! define the precision to be used, and other constant values
 
   implicit none
 
   integer, parameter :: dp = selected_real_kind(15,307)
 
-! user constants
+  ! user constants
   real(dp), parameter :: zero = 0.0_dp, One = 1.0_dp 
   real(dp), parameter :: pi = 3.14159265358979_dp
   real(dp), parameter, dimension(3) :: e1 = (/One, zero, zero/), &
@@ -13,4 +13,4 @@
                        e3 = (/zero, zero, One/) ! unit vectors 
   
 
-  end module precision
+end module precision
