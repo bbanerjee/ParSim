f061b5dc61c21154874b37d4eabd143e4b44214a
Hooman Zarreh <hooman>
2013-10-06 17:59:38 +1300

    convert datawarehouse.py to C++

---
+++ BrMPM/src/MPMdatawarehouse.h
@@ -0,0 +1,59 @@
+#ifndef __MATITI_MPMDATAWAREHOUSE__
+#define __MATITI_MPMDATAWAREHOUSE__
+
+
+#include <Geometry/Point3D>
+#include <Geometry/Vector3D>
+#include <Output.h>
+#include <OutputVTK.h>
+#include <MPMTime.h>
+
+#include <vector>
+#include <iostream>
+#include <string>
+#include <>
+
+namespace Matiti {
+  
+  class MPMdatawarehouse.h {
+
+  public:
+     MPMdatawarehouse();
+     ~MPMdatawarehouse();
+
+
+
+
+
+  private:
+
+
+
+    OutputVTK d_out;
+    MPMTime d_time;
+    MPMsaveutil d_save;
+
+
+    int d_id;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
