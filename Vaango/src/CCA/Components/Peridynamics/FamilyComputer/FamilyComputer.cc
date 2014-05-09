61332fe7f2cfb3b8249b188a51f7b52118dcf21f
bbanerjee <b.banerjee.nz@gmail.com>
2014-05-09 16:46:33 +1200

    Added cell search.

--- Vaango/src/CCA/Components/Peridynamics/FamilyComputer/FamilyComputer.cc
+++ Vaango/src/CCA/Components/Peridynamics/FamilyComputer/FamilyComputer.cc
@@ -67,11 +67,46 @@ FamilyComputer::createNeighborList(PeridynamicsMaterial* matl,
   }
 }
 
+// Compute the min max cells which intersect the horizon ball
+// Algorithm:  Let p be the point and let d = (dx, dy, dz) be the diagonal of a grid cell
+//             Then the unit vectors along the three coordinate directions are
+//             \hat{v}_x = (1,0,0) ,  \hat{v}_y = (0,1,0) ,  \hat{v}_z = (0,0,1)
+//             The vectors going from the point to the max and min cells are then
+//             v_x = h  \hat{v}_x, v_y = h  \hat{v}_y, v_z = h  \hat{v}_z, 
+//             The points in the min max cells are 
+//             p_x_max = p + v_x , p_x_min = p - v_x 
+//             p_y_max = p + v_y , p_y_min = p - v_y 
+//             p_z_max = p + v_z , p_z_min = p - v_z 
+// Returns: a vector of cells in the following order:
+//            cell(p), cell(p_x_max), cell(p_y_max), cell(p_z_max),
+//            cell(p_x_min), cell(p_y_min), cell(p_z_min)
 void
 FamilyComputer::findCellsInHorizon(const Uintah::Patch* patch,
                                    const SCIRun::Point& pos,
                                    const double& horizon,
                                    std::vector<SCIRun::IntVector>& cells)
 {
-  //SCIRun::Point cellpos = patch->getLevel()->positionToIndex(pos);
+  // Find the cell in which the particle resides
+  SCIRun::IntVector cellIndex = patch->getLevel()->getCellIndex(pos);
+  cells.push_back(cellIndex);
+
+  SCIRun::Vector vx(horizon, 0.0, 0.0), vy(0.0, horizon, 0.0), vz(0.0, 0.0, horizon);
+  SCIRun::Point pxMax = pos + vx;
+  SCIRun::Point pyMax = pos + vy;
+  SCIRun::Point pzMax = pos + vz;
+  SCIRun::IntVector cellPxMax = patch->getLevel()->getCellIndex(pxMax);
+  SCIRun::IntVector cellPyMax = patch->getLevel()->getCellIndex(pyMax);
+  SCIRun::IntVector cellPzMax = patch->getLevel()->getCellIndex(pzMax);
+  cells.push_back(cellPxMax);
+  cells.push_back(cellPyMax);
+  cells.push_back(cellPzMax);
+  SCIRun::Point pxMin = pos - vx;
+  SCIRun::Point pyMin = pos - vy;
+  SCIRun::Point pzMin = pos - vz;
+  SCIRun::IntVector cellPxMin = patch->getLevel()->getCellIndex(pxMin);
+  SCIRun::IntVector cellPyMin = patch->getLevel()->getCellIndex(pyMin);
+  SCIRun::IntVector cellPzMin = patch->getLevel()->getCellIndex(pzMin);
+  cells.push_back(cellPxMin);
+  cells.push_back(cellPyMin);
+  cells.push_back(cellPzMin);
 }
