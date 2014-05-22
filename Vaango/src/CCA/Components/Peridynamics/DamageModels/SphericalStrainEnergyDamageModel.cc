a34e0a39d71b10747655f708d8f464c7d77a9835
bbanerjee <b.banerjee.nz@gmail.com>
2014-05-22 17:27:39 +1200

    This is just a backup of the latest incomplete version of the Damage model implementation.

--- Vaango/src/CCA/Components/Peridynamics/DamageModels/SphericalStrainEnergyDamageModel.cc
+++ Vaango/src/CCA/Components/Peridynamics/DamageModels/SphericalStrainEnergyDamageModel.cc
@@ -24,8 +24,15 @@ using Uintah::PatchSubset;
 using Uintah::DataWarehouse;
 using Uintah::ParticleSubset;
 using Uintah::ParticleVariable;
+using Uintah::constParticleVariable;
 using Uintah::Matrix3;
+using Uintah::NeighborList;
+using Uintah::NeighborConnectivity;
+using Uintah::NeighborBondEnergy;
 using Uintah::Ghost;
+using SCIRun::Point;
+using Uintah::particleIndex;
+using Uintah::long64;
 
 SphericalStrainEnergyDamageModel::SphericalStrainEnergyDamageModel(ProblemSpecP& ps,
                                                                    PeridynamicsLabel* labels,
@@ -130,6 +137,61 @@ SphericalStrainEnergyDamageModel::computeDamageTensor(const PatchSubset* patches
                                                       DataWarehouse* old_dw,
                                                       DataWarehouse* new_dw)
 {
+
+  // Loop through patches
+  for (int pp = 0; pp < patches->size(); pp++) {
+
+    // Get the current patch
+    const Patch* patch = patches->get(pp);
+
+    // Get the index of current material in the data warehouse
+    int matlIndex = matl->getDWIndex();
+
+    // Get the particle subset for this material in this patch
+    ParticleSubset* pset = old_dw->getParticleSubset(matlIndex, patch);
+
+    // Get the particle data needed for damage computation
+    constParticleVariable<Point> pPosition;
+    old_dw->get(pPosition, d_label->pPositionLabel, pset);
+
+    constParticleVariable<long64> pParticleID;
+    old_dw->get(pParticleID, d_label->pParticleIDLabel, pset);
+
+    constParticleVariable<double> pHorizon;
+    old_dw->get(pHorizon, d_label->pHorizonLabel, pset);
+
+    constParticleVariable<Matrix3> pStress_new;
+    new_dw->get(pStress_new, d_label->pStressLabel_preReloc, pset);
+
+    constParticleVariable<int> pNeighborCount;
+    old_dw->get(pNeighborCount, d_label->pNeighborCountLabel, pset);
+
+    constParticleVariable<NeighborList> pNeighborList;
+    old_dw->get(pNeighborList, d_label->pNeighborListLabel, pset);
+
+    constParticleVariable<NeighborConnectivity> pNeighborConn;
+    old_dw->get(pNeighborConn, d_label->pNeighborConnLabel, pset);
+
+    constParticleVariable<NeighborBondEnergy> pNeighborBondEnergy;
+    old_dw->get(pNeighborBondEnergy, d_label->pNeighborBondEnergyLabel, pset);
+
+    // Initialize variables that will be updated in this task
+    ParticleVariable<NeighborConnectivity> pNeighborConn_new;
+    new_dw->allocateAndPut(pNeighborConn_new, d_label->pNeighborConnLabel_preReloc, pset);
+
+    ParticleVariable<NeighborBondEnergy> pNeighborBondEnergy_new;
+    new_dw->allocateAndPut(pNeighborBondEnergy_new, d_label->pNeighborBondEnergyLabel_preReloc, pset);
+
+    ParticleVariable<Matrix3> pDamage_new;;
+    new_dw->allocateAndPut(pDamage_new, d_label->pDamageLabel_preReloc, pset);
+
+    // Loop through particles
+    for (ParticleSubset::iterator iter = pset->begin(); iter != pset->end(); iter++) {
+
+      // Get particle index
+      particleIndex idx = *iter;
+    }
+  }  // End patch loop
 }
 
 
