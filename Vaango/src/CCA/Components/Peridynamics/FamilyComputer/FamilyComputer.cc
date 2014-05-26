77b46e16130b6edeacdca5f6622b09b7d79649e5
bbanerjee <b.banerjee.nz@gmail.com>
2014-05-26 22:22:16 +1200

    Make sure patch limits are respected when searching for particles in horizon.

--- Vaango/src/CCA/Components/Peridynamics/FamilyComputer/FamilyComputer.cc
+++ Vaango/src/CCA/Components/Peridynamics/FamilyComputer/FamilyComputer.cc
@@ -18,6 +18,23 @@
 
 using namespace Vaango;
 
+using Uintah::Task;
+using Uintah::Patch;
+using Uintah::PatchSet;
+using Uintah::DataWarehouse;
+using Uintah::ParticleSubset;
+using Uintah::MaterialSubset;
+using Uintah::Ghost;
+using Uintah::ParticleVariable;
+using Uintah::constParticleVariable;
+using Uintah::long64;
+using Uintah::NeighborList;
+using Uintah::NeighborConnectivity;
+using Uintah::NeighborBondEnergy;
+using SCIRun::Point;
+using SCIRun::IntVector;
+using SCIRun::Vector;
+
 //__________________________________
 //  To turn on debug flags
 //  csh/tcsh : setenv SCI_DEBUG "PDFamDoing:+,PDFamDebug:+".....
@@ -41,21 +58,21 @@ FamilyComputer::~FamilyComputer()
 
 /*! Initial computes and requires for the family computer */
 void 
-FamilyComputer::addInitialComputesAndRequires(Uintah::Task* task,
+FamilyComputer::addInitialComputesAndRequires(Task* task,
                                               const PeridynamicsMaterial* matl,
-                                              const Uintah::PatchSet* patches) const
+                                              const PatchSet* patches) const
 {
   cout_doing << "\t Scheduling task variables in family computer: Peridynamics: " 
              << __FILE__ << ":" << __LINE__ << std::endl;
 
   // Identify this material
-  const Uintah::MaterialSubset* matlset = matl->thisMaterial();
+  const MaterialSubset* matlset = matl->thisMaterial();
 
   // The quantities that are required by this task
-  Uintah::Ghost::GhostType  gn = Uintah::Ghost::None;
-  task->requires(Uintah::Task::OldDW, d_label->pPositionLabel,    gn);
-  task->requires(Uintah::Task::OldDW, d_label->pParticleIDLabel,  gn);
-  task->requires(Uintah::Task::OldDW, d_label->pHorizonLabel,     gn);
+  Ghost::GhostType  gn = Ghost::None;
+  task->requires(Task::OldDW, d_label->pPositionLabel,    gn);
+  task->requires(Task::OldDW, d_label->pParticleIDLabel,  gn);
+  task->requires(Task::OldDW, d_label->pHorizonLabel,     gn);
 
   // The quantities that are computed in this task
   task->computes(d_label->pNeighborListLabel, matlset);
@@ -66,8 +83,8 @@ FamilyComputer::addInitialComputesAndRequires(Uintah::Task* task,
 
 void
 FamilyComputer::createNeighborList(PeridynamicsMaterial* matl,
-                                   const Uintah::Patch* patch,
-                                   Uintah::DataWarehouse* new_dw)
+                                   const Patch* patch,
+                                   DataWarehouse* new_dw)
 {
   cout_doing << "\t Creating Neighbor list: Peridynamics: " << __FILE__ << ":" << __LINE__ << std::endl;
 
@@ -77,24 +94,24 @@ FamilyComputer::createNeighborList(PeridynamicsMaterial* matl,
   // Get the subset of particles in this patch
   // This subset should contain all particles within the horizon of a particle.  
   // The number of extra cells to be carried around will therefore depend on the horizon
-  Uintah::ParticleSubset* pset = new_dw->getParticleSubset(matlIndex, patch);
+  ParticleSubset* pset = new_dw->getParticleSubset(matlIndex, patch);
 
   // Get the positions and horizons of the particles from the new data warehouse
-  Uintah::constParticleVariable<SCIRun::Point> pPosition;
+  constParticleVariable<Point> pPosition;
   new_dw->get(pPosition, d_label->pPositionLabel, pset);
 
-  Uintah::constParticleVariable<double> pHorizon;
+  constParticleVariable<double> pHorizon;
   new_dw->get(pHorizon, d_label->pHorizonLabel, pset);
 
   // Get the particle IDs from the data warehouse
-  Uintah::constParticleVariable<Uintah::long64> pParticleID;
+  constParticleVariable<long64> pParticleID;
   new_dw->get(pParticleID, d_label->pParticleIDLabel, pset);
 
   // Create allocation for the family of each particle
-  Uintah::ParticleVariable<Uintah::NeighborList> pNeighborList;
-  Uintah::ParticleVariable<Uintah::NeighborConnectivity> pNeighborConn;
-  Uintah::ParticleVariable<int> pNeighborCount;
-  Uintah::ParticleVariable<Uintah::NeighborBondEnergy> pNeighborBondEnergy;
+  ParticleVariable<NeighborList> pNeighborList;
+  ParticleVariable<NeighborConnectivity> pNeighborConn;
+  ParticleVariable<int> pNeighborCount;
+  ParticleVariable<NeighborBondEnergy> pNeighborBondEnergy;
   new_dw->allocateAndPut(pNeighborList, d_label->pNeighborListLabel, pset);
   new_dw->allocateAndPut(pNeighborConn, d_label->pNeighborConnLabel, pset);
   new_dw->allocateAndPut(pNeighborCount, d_label->pNeighborCountLabel, pset);
@@ -105,28 +122,34 @@ FamilyComputer::createNeighborList(PeridynamicsMaterial* matl,
   new_dw->createParticleIDMap(pset, d_label->pParticleIDLabel, idMap);
 
   if (cout_dbg.active()) {
-    cout_dbg << "\t" << "ParticleID <-> Particle index Map for patch" << patch << std::endl;
+    cout_dbg << "\t" << "ParticleID <-> Particle index Map for patch " << patch << std::endl;
     for (auto iter = idMap.begin(); iter != idMap.end(); iter++) {
       cout_dbg << "\t\t" << " ID = " << iter->first << " index = " << iter->second << std::endl;
     }
   }
 
   // Loop through the particle list
-  Uintah::ParticleSubset::iterator iter = pset->begin();
+  ParticleSubset::iterator iter = pset->begin();
   for (; iter != pset->end(); iter++) {
     particleIndex idx = *iter;
 
     // Find the cell in which this particle sits and its neighbors within the
     // horizon
-    SCIRun::IntVector cellLow, cellHigh;
+    IntVector cellLow, cellHigh;
     findCellsInHorizon(patch, pPosition[idx], pHorizon[idx], cellLow, cellHigh);
 
+    if (cout_dbg.active()) {
+      cout_dbg << "\t Particle index = " << idx << " position = " << pPosition[idx]
+               << " horizon = " << pHorizon[idx] << std::endl;
+      cout_dbg << "\t Cells in horizon: low = " << cellLow << " high = " << cellHigh << std::endl;
+    }
+
     // Now that the cells have been found, create a list of particles that reside within these cells
-    Uintah::ParticleSubset* pFamilySet = new_dw->getParticleSubset(matlIndex, patch, cellLow, cellHigh);
+    ParticleSubset* pFamilySet = new_dw->getParticleSubset(matlIndex, patch, cellLow, cellHigh);
 
     // Get the positions and particle IDs of the particles from the new data warehouse
-    Uintah::constParticleVariable<SCIRun::Point> pFamilyPos;
-    Uintah::constParticleVariable<Uintah::long64> pFamilyPID;
+    constParticleVariable<Point> pFamilyPos;
+    constParticleVariable<long64> pFamilyPID;
     new_dw->get(pFamilyPos, d_label->pPositionLabel, pFamilySet);
     new_dw->get(pFamilyPID, d_label->pParticleIDLabel, pFamilySet);
 
@@ -134,14 +157,14 @@ FamilyComputer::createNeighborList(PeridynamicsMaterial* matl,
     std::vector<Uintah::ParticleID> family;
 
     // Loop through the (potential) family particle set
-    Uintah::ParticleSubset::iterator pFamilyIter = pFamilySet->begin();
+    ParticleSubset::iterator pFamilyIter = pFamilySet->begin();
     for (; pFamilyIter != pFamilySet->end(); pFamilyIter++) {
       particleIndex pFamilyIdx = *pFamilyIter;
 
       // Calculate distance from particle to family particle
       if (pParticleID[idx] != pFamilyPID[pFamilyIdx]) {
 
-	SCIRun::Vector bondVector = pFamilyPos[pFamilyIdx] - pPosition[idx];
+	Vector bondVector = pFamilyPos[pFamilyIdx] - pPosition[idx];
         double horizon = pHorizon[idx];
         if (!(bondVector.length2() > horizon*horizon)) {
           family.push_back(pFamilyPID[pFamilyIdx]);
@@ -150,9 +173,9 @@ FamilyComputer::createNeighborList(PeridynamicsMaterial* matl,
     }
 
     // Fill the neighbor information
-    Uintah::NeighborList neighbors(family);
-    Uintah::NeighborConnectivity intact;
-    Uintah::NeighborBondEnergy bondEnergy;
+    NeighborList neighbors(family);
+    NeighborConnectivity intact;
+    NeighborBondEnergy bondEnergy;
     pNeighborList[idx] = neighbors;
     pNeighborConn[idx] = intact;
     pNeighborBondEnergy[idx] = bondEnergy;
@@ -177,31 +200,46 @@ FamilyComputer::createNeighborList(PeridynamicsMaterial* matl,
 //             p_z_max = p + v_z , p_z_min = p - v_z 
 // Returns: The lowest and highest cell indices in the range
 void
-FamilyComputer::findCellsInHorizon(const Uintah::Patch* patch,
-                                   const SCIRun::Point& pos,
+FamilyComputer::findCellsInHorizon(const Patch* patch,
+                                   const Point& pos,
                                    const double& horizon,
-                                   SCIRun::IntVector& cellLow,
-                                   SCIRun::IntVector& cellHigh)
+                                   IntVector& cellLow,
+                                   IntVector& cellHigh)
 {
-  SCIRun::Vector vx(horizon, 0.0, 0.0), vy(0.0, horizon, 0.0), vz(0.0, 0.0, horizon);
+  // Get the index range for this patch
+  IntVector lowIndex, highIndex;
+  patch->getLevel()->findCellIndexRange(lowIndex, highIndex);
+  if (cout_dbg.active()) {
+    cout_dbg << "\t Patch: Low index = " << lowIndex << " High index = " << highIndex << std::endl;
+  }
 
-  SCIRun::Point pxMin = pos - vx;
-  SCIRun::Point pyMin = pos - vy;
-  SCIRun::Point pzMin = pos - vz;
-  SCIRun::IntVector cellPxMin = patch->getLevel()->getCellIndex(pxMin);
-  SCIRun::IntVector cellPyMin = patch->getLevel()->getCellIndex(pyMin);
-  SCIRun::IntVector cellPzMin = patch->getLevel()->getCellIndex(pzMin);
-  cellLow.x(cellPxMin.x());
-  cellLow.y(cellPyMin.y());
-  cellLow.z(cellPzMin.z());
+  // Set the horizon range vectors
+  Vector vx(horizon, 0.0, 0.0), vy(0.0, horizon, 0.0), vz(0.0, 0.0, horizon);
 
-  SCIRun::Point pxMax = pos + vx;
-  SCIRun::Point pyMax = pos + vy;
-  SCIRun::Point pzMax = pos + vz;
-  SCIRun::IntVector cellPxMax = patch->getLevel()->getCellIndex(pxMax);
-  SCIRun::IntVector cellPyMax = patch->getLevel()->getCellIndex(pyMax);
-  SCIRun::IntVector cellPzMax = patch->getLevel()->getCellIndex(pzMax);
-  cellHigh.x(cellPxMax.x());
-  cellHigh.y(cellPyMax.y());
-  cellHigh.z(cellPzMax.z());
+  Point pxMin = pos - vx;
+  Point pyMin = pos - vy;
+  Point pzMin = pos - vz;
+  IntVector cellPxMin = patch->getLevel()->getCellIndex(pxMin);
+  IntVector cellPyMin = patch->getLevel()->getCellIndex(pyMin);
+  IntVector cellPzMin = patch->getLevel()->getCellIndex(pzMin);
+
+  // If the computed min are outside the patch limits, set them to the
+  // limits
+  cellLow.x(cellPxMin.x() < lowIndex.x() ? lowIndex.x() : cellPxMin.x());
+  cellLow.y(cellPyMin.y() < lowIndex.y() ? lowIndex.y() : cellPxMin.y());
+  cellLow.z(cellPzMin.z() < lowIndex.z() ? lowIndex.z() : cellPxMin.z());
+
+  Point pxMax = pos + vx;
+  Point pyMax = pos + vy;
+  Point pzMax = pos + vz;
+  IntVector cellPxMax = patch->getLevel()->getCellIndex(pxMax);
+  IntVector cellPyMax = patch->getLevel()->getCellIndex(pyMax);
+  IntVector cellPzMax = patch->getLevel()->getCellIndex(pzMax);
+
+  // If the computed max are outside the patch limits, set them to the
+  // limits
+  cellHigh.x(cellPxMax.x() > highIndex.x() ? highIndex.x() : cellPxMax.x());
+  cellHigh.y(cellPyMax.y() > highIndex.y() ? highIndex.y() : cellPyMax.y());
+  cellHigh.z(cellPzMax.z() > highIndex.z() ? highIndex.z() : cellPzMax.z());
+
 }
