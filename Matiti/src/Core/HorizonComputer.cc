c4e2baa527a0ae6567e460f4bab0e8134e1d75c6
hoomanZ <hzar193@aucklanduni.ac.nz>
2014-09-22 15:55:34 +1200

    Modify the calcualtion of horizon and add thr setInterval function.

--- Matiti/src/Core/HorizonComputer.cc
+++ Matiti/src/Core/HorizonComputer.cc
@@ -23,6 +23,10 @@ HorizonComputer::operator()(const BodySP body,
     ElementPArray adj_elems = cur_node->getAdjacentElements();
 
     double max_length = 0.0;
+    double max_length_xyz = 0.0;
+    double max_length_x = 0.0;
+    double max_length_y = 0.0;
+    double max_length_z = 0.0;
 
     // Loop thru elements attached to this node
     for (auto elem_iter = adj_elems.begin(); elem_iter != adj_elems.end(); ++elem_iter) {
@@ -34,12 +38,55 @@ HorizonComputer::operator()(const BodySP body,
                 elem_node_iter != cur_elem_nodes.end(); ++elem_node_iter) {
           
         NodeP cur_elem_node = *elem_node_iter;
+
+        double dist_x = std::abs(cur_node->x()-cur_elem_node->x());
+        double dist_y = std::abs(cur_node->y()-cur_elem_node->y());
+        double dist_z = std::abs(cur_node->z()-cur_elem_node->z());
+
         double dist = cur_node->distance(*cur_elem_node);
         max_length = std::max(max_length, dist);
+
+        max_length_x = std::max(max_length_x, dist_x);
+        max_length_y = std::max(max_length_y, dist_y);
+        max_length_z = std::max(max_length_z, dist_z);
+
+        max_length_xyz = std::max(std::max(max_length_x, max_length_y), max_length_z);
       }
     }
-    cur_node->horizonSize(state.horizonFactor()*max_length/COEF);
+   
+//   std::cout << std::endl << std::endl << "Max_length= " << max_length << "   Max_length_xyz= " << max_length_xyz 
+//             << "  horizon_factor= " << state.horizonFactor() << std::endl << std::endl;
+   
+    //cur_node->horizonSize(state.horizonFactor()*max_length/COEF);
+    
+//    cur_node->horizonSize(state.horizonFactor()*max_length);
+     
+     cur_node->horizonSize(state.horizonFactor()*max_length_xyz);
+
+     Array3 interval = {{0.0, 0.0, 0.0}};
+     interval[0] = max_length_x;  
+     interval[1] = max_length_y;
+     interval[2] = max_length_z;
+     cur_node->setInterval(interval);  
+//   std::cout << "node's_horizon_size= " << cur_node->horizonSize() << std::endl;
+//   std::cout << "node's interval vector= (" << cur_node->getInterval()[0] << ", "
+//                                            << cur_node->getInterval()[1] << ", " 
+//                                            << cur_node->getInterval()[2] << ")" << std::endl;                           
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 
