11c35f2a2ff491f01fd30c2f3d367b0b3e372771
Biswajit Banerjee <b.banerjee.nz@gmail.com>
2014-11-30 18:34:26 +1300

    Updated extractors.

--- Vaango/src/StandAlone/partvarRange.cc
+++ Vaango/src/StandAlone/partvarRange.cc
@@ -1,31 +1,8 @@
 /*
  * The MIT License
  *
- * Copyright (c) 2013-2014 Callaghan Innovation, New Zealand
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to
- * deal in the Software without restriction, including without limitation the
- * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
- * sell copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
- * IN THE SOFTWARE.
- */
-
-/*
- * The MIT License
- *
  * Copyright (c) 1997-2012 The University of Utah
+ * Copyright (c) 2013-2014 Callaghan Innovation, New Zealand
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
@@ -159,6 +136,8 @@ int main(int argc, char** argv)
             unsigned long time_step_lower = 0;
             unsigned long time_step_upper = times.size()-1;
             unsigned long t=time_step_lower;
+            int numParticles = 0;
+            int numSteps = times.size();
             for(; t<=time_step_upper; t++){
               GridP grid = da->queryGrid(t);
 
@@ -186,15 +165,19 @@ int main(int argc, char** argv)
                     if(pset->numParticles() > 0){
                       ParticleSubset::iterator iter = pset->begin();
                       for(;iter != pset->end(); iter++){
+                        numParticles++;
                         min=Min(min, value[*iter]);
                         max=Max(max, value[*iter]);
                       }
+                      //std::cout << "numParticles = " << numParticles << std::endl;
                     }
                   } // end material loop
                 } // end patch loop
               } // end level loop
             } // end time loop
-            cout << var << " min = " << min << " max = " << max << endl;
+            numParticles /= numSteps;
+            cout << "# particles = " << numParticles << " " 
+                 << var << " min = " << min << " max = " << max << endl;
           } // end double case
         break;
 
