abdff9e7d954dec16591ccfe996a5d9634b8919c
Biswajit Banerjee <b.banerjee.nz@gmail.com>
2020-09-15 20:38:49 +1200

    Pushing first cut of support vector model.  Does not compile yet.

---
+++ Vaango/src/CCA/Components/MPM/ConstitutiveModel/ElasticModuliModels/ElasticModuli_SupportVector.cc
@@ -0,0 +1,467 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2015-2020 Parresia Research Limited, New Zealand
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include <CCA/Components/MPM/ConstitutiveModel/ElasticModuliModels/ElasticModuli_SupportVector.h>
+#include <CCA/Components/MPM/ConstitutiveModel/ModelState/ModelState_Tabular.h>
+#include <Core/Exceptions/InternalError.h>
+#include <Core/Exceptions/InvalidValue.h>
+#include <Core/Exceptions/ProblemSetupException.h>
+
+#define DEBUG_SVR
+
+using namespace Vaango;
+
+/** Read inputs and JSON */
+ElasticModuli_SupportVector::ElasticModuli_SupportVector(Uintah::ProblemSpecP& ps)
+{
+  ps->require("svr_filename", d_bulk.filename);
+  ps->require("G0", d_shear.G0);
+  ps->require("nu", d_shear.nu);
+
+  readJSONFile(d_bulk.filename);
+  checkInputParameters();
+}
+
+/** Check that the input parameters are reasonable */
+void
+ElasticModuli_SupportVector::checkInputParameters()
+{
+  std::ostringstream warn;
+  if (d_shear.G0 <= 0.0) {
+    warn << "G0 must be positive. G0 = " << d_shear.G0 << std::endl;
+    throw Uintah::ProblemSetupException(warn.str(), __FILE__, __LINE__);
+  }
+}
+
+/** Construct a copy of the model */
+ElasticModuli_SupportVector::ElasticModuli_SupportVector(const ElasticModuli_SupportVector* model)
+{
+  d_bulk = model->d_bulk;
+  d_shear = model->d_shear;
+}
+
+/** Write out info for restart files */
+void
+ElasticModuli_SupportVector::outputProblemSpec(Uintah::ProblemSpecP& ps)
+{
+  Uintah::ProblemSpecP elasticModuli_ps = ps->appendChild("elastic_moduli_model");
+  elasticModuli_ps->setAttribute("type", "support_vector");
+
+  elasticModuli_ps->appendElement("svr_filename", d_bulk.filename);
+  elasticModuli_ps->appendElement("G0", d_shear.G0);
+  elasticModuli_ps->appendElement("nu", d_shear.nu);
+}
+
+/** Compute initial elastic moduli */
+ElasticModuli
+ElasticModuli_SupportVector::getInitialElasticModuli() const
+{
+  double K = computeBulkModulus(1.0e-6, 0);
+  double G = computeShearModulus(K);
+  return ElasticModuli(K, G);
+}
+
+/** Compute current elastic moduli */
+ElasticModuli
+ElasticModuli_SupportVector::getCurrentElasticModuli(const ModelStateBase* state_input) const
+{
+  // This model requires the extra variables in ModelState_Tabular
+  const ModelState_Tabular* state =
+    static_cast<const ModelState_Tabular*>(state_input);
+  /*
+  if (!state) {
+    std::ostringstream out;
+    out << "**ERROR** The correct ModelState object has not been passed."
+        << " Need ModelState_Tabular.";
+    throw Uintah::InternalError(out.str(), __FILE__, __LINE__);
+  }
+  */
+
+  // Make sure the quantities are positive in compression
+  double ev_e_bar = -(state->elasticStrainTensor).Trace();
+  double ev_p_bar = -(state->plasticStrainTensor).Trace();
+  ev_p_bar = (ev_p_bar < 0) ? 0 : ev_p_bar;
+  ev_e_bar = (ev_p_bar < 0) ? ev_e_bar + ev_p_bar : ev_e_bar;
+
+  // Compute the elastic moduli
+  double K = computeBulkModulus(ev_e_bar, ev_p_bar);
+  double G = computeShearModulus(K);
+
+  #ifdef DEBUG_SVR
+    if (K < 0 || !std::isfinite(K)) {
+      std::cout << "ev_e = " << ev_e_bar << " ev_p = " << ev_p_bar << "\n";
+      std::cout << " K = " << K << " G = " << G << std::endl;
+    }
+  #endif
+
+  return ElasticModuli(K, G);
+}
+
+/** Compute the bulk modulus */
+double 
+ElasticModuli_SupportVector::computeBulkModulus(const double& elasticVolStrain,
+                                                const double& plasticVolStrain) const
+{
+  Eigen::Vector2d strain;
+  strain << elasticVolSrain+plasticVolStrain, plasticVolStrain;
+  strain *= d_bulk.strain_conversion_factor;
+
+  auto strain_scaled = (strain - d_bulk.strain_min) * d_bulk.strain_scale_factor;
+  auto strain_svr_diff = d_bulk.svr_support_vectors - strain_scaled;
+  auto strain_svr_diff_normSq = strain_svr_diff.rowwise().squaredNorm() * (-rbf_kernel_gamma);
+  auto svr_kernel = strain_svr_diff_normSq.array().exp();
+  auto strain_svr_diff_gamma = strain_svr_diff.col(0) * (2.0 * rbf_kernel_gamma);
+  auto svr_kernel_deriv_totalstrain = svr_kernel.array() * strain_svr_diff_gamma.array();
+  svr_kernel_deriv_totalstrain /= d_bulk.pressure_scale_factor;
+
+  double K = d_bulk.svr_dual_coeffs * svr_kernel_deriv_totalstrain.transpose();
+
+  #ifdef DEBUG_SVR
+    if (K < 0 || !std::isfinite(K)) {
+      std::cout <<std::setprecision(16) 
+                << "ee_v = " << elasticVolStrain
+                << " ep_v = " << plasticVolStrain
+                << " K = " << K << "\n";
+    }
+  #endif
+
+  return K;
+}
+
+double 
+ElasticModuli_SupportVector::computeShearModulus(const double& K) const
+{
+  double nu = d_shear.nu;
+  double G = (nu > -1.0 && nu < 0.5) 
+             ? 1.5*K*(1.0 - 2.0*nu)/(1.0 + nu) 
+             : d_shear.G0;
+  return G;
+}
+
+/* Get the elastic moduli and their derivatives with respect to a single
+   plastic internal variable */
+std::pair<ElasticModuli, ElasticModuli>
+ElasticModuli_SupportVector::getElasticModuliAndDerivatives(const ModelStateBase* state_input) const
+{
+  const ModelState_Tabular* state =
+    dynamic_cast<const ModelState_Tabular*>(state_input);
+  //if (!state) {
+  //  std::ostringstream out;
+  //  out << "**ERROR** The correct ModelState object has not been passed."
+  //      << " Need ModelState_Tabular.";
+  //  throw Uintah::InternalError(out.str(), __FILE__, __LINE__);
+  //}
+
+  Eigen::Vector2d strain;
+  strain << elasticVolSrain+plasticVolStrain, plasticVolStrain;
+  strain *= d_bulk.strain_conversion_factor;
+
+  auto strain_scaled = (strain - d_bulk.strain_min) * d_bulk.strain_scale_factor;
+  auto strain_svr_diff = d_bulk.svr_support_vectors - strain_scaled;
+  auto strain_svr_diff_normSq = strain_svr_diff.rowwise().squaredNorm() * (-d_bulk.rbf_kernel_gamma);
+  auto svr_kernel = strain_svr_diff_normSq.array().exp();
+  auto strain_svr_diff_gamma = strain_svr_diff.col(0) * (2.0 * d_bulk.rbf_kernel_gamma);
+  auto svr_kernel_deriv_totalstrain = svr_kernel.array() * strain_svr_diff_gamma.array();
+  svr_kernel_deriv_totalstrain /= d_bulk.pressure_scale_factor;
+
+  auto strain_svr_diffsq = strain_svr_diff.col(1).array() * strain_svr_diff.col(1).array();
+  auto strain_svr_diffsq_gamma = (strain_svr_diffsq * (2.0 * d_bulk.rbf_kernel_gamma) - 1) * (2.0 * d_bulk.rbf_kernel_gamma);
+  auto svr_kernel_dderiv_plasticstrain = svr_kernel.array() * strain_svr_diffsq_gamma.array();
+  svr_kernel_dderiv_plasticstrain /= d_bulk.pressure_scale_factor;
+
+  double K = d_bulk.svr_dual_coeffs * svr_kernel_deriv_totalstrain.transpose();
+  double dK_deps_p = d_bulk.svr_dual_coeffs * svr_kernel_deriv_plasticstrain.transpose();
+
+  double nu = d_shear.nu;
+  double G = (nu > -1.0 && nu < 0.5) 
+             ? 1.5*K*(1.0 - 2.0*nu)/(1.0 + nu) 
+             : d_shear.G0;
+  double dG_deps_p = (nu > -1.0 && nu < 0.5) 
+             ? 1.5*dK_deps_p*(1.0 - 2.0*nu)/(1.0 + nu) 
+             : 0.0;
+
+  return std::make_pair(ElasticModuli(K, G),
+                        ElasticModuli(dK_deps_p, dG_deps_p));
+}
+
+/*! Compute derivatives of moduli with respect to internal variables */
+std::vector<ElasticModuli> 
+ElasticModuli_SupportVector::computeDModuliDIntVar(const ModelStateBase* state) const
+{
+  auto data = computeModuliAndDModuliDIntVar(state);
+  std::vector<ElasticModuli> derivs;
+  derivs.push_back(data.second);
+  return derivs;
+}
+
+/*! Compute moduli and derivatives of moduli with respect to internal variables */
+std::pair<ElasticModuli, std::vector<ElasticModuli>>
+ElasticModuli_SupportVector::computeModuliAndDModuliDIntVar(const ModelStateBase* state) const
+{
+  auto data = getElasticModuliAndDerivatives(state);
+  return data;
+}
+
+void
+ElasticModuli_SupportVector::readJSONFile(const std::string& filename)
+{
+  std::ifstream inputFile(filename);
+  if (!inputFile) {
+    std::ostringstream out;
+    out << "**ERROR**"
+        << " Cannot read support vector regression JSON file " << filename;
+    throw Uintah::ProblemSetupException(out.str(), __FILE__, __LINE__);
+  }
+
+  std::stringstream inputStream;
+  inputStream << inputFile.rdbuf();
+  inputFile.close();
+
+  nlohmann::json doc = loadJSON(inputStream, filename);
+  
+  d_bulk.strain_conversion_factor = getVector2dJSON(doc, "X_conversion_factor", filename); 
+  d_bulk.strain_scale_factor = getVector2dJSON(doc, "X_scale", filename); 
+  d_bulk.strain_min = getVector2dJSON(doc, "X_min", filename); 
+  d_bulk.strain_max = getVector2dJSON(doc, "X_max", filename); 
+  d_bulk.pressure_conversion_factor = getVector1dJSON(doc, "y_conversion_factor", filename); 
+  d_bulk.pressure_scale_factor = getVector1dJSON(doc, "y_scale", filename); 
+  d_bulk.pressure_min = getVector1dJSON(doc, "y_min", filename); 
+  d_bulk.pressure_max = getVector1dJSON(doc, "y_max", filename); 
+  d_bulk.rbf_kernel_gamma = getDoubleJSON(doc, "gamma", filename);
+  d_bulk.svr_support_vectors = getMatrixN2dJSON(doc, "support_vectors", filename);
+  d_bulk.svr_dual_coeffs = getMatrixN1dJSON(doc, "dual_coeffs", filename);
+  d_bulk.svr_intercept = getVector1dJSON(doc, "intercept", filename); 
+}
+
+nlohmann::json 
+ElasticModuli_SupportVector::loadJSON(std::stringstream& inputStream,
+                                      const std::string& filename) const
+{
+  nlohmann::json doc;
+  try {
+    doc << inputStream;
+  } catch (std::invalid_argument err) {
+    std::ostringstream out;
+    out << "**ERROR**"
+        << " Cannot parse support vector regression JSON file " << filename << "\n"
+        << " Please check that the file is valid JSON using a linter";
+    throw Uintah::ProblemSetupException(out.str(), __FILE__, __LINE__);
+  }
+  return doc;
+}
+
+double 
+ElasticModuli_SupportVector::getDoubleJSON(const nlohmann::json& object, 
+                                           const std::string key,
+                                           const std::string& filename) const
+{
+  // First check if the key exists
+  nlohmann::json data;
+  try {
+    data = object.at(key);
+    // std::cout << "Data = " << data << std::endl;
+  } catch (std::out_of_range err) {
+    std::ostringstream out;
+    out << "**ERROR**"
+        << " Variable \"" << key << "\" not found in support vector regression JSON file "
+        << filename;
+    throw Uintah::ProblemSetupException(out.str(), __FILE__, __LINE__);
+  }
+
+  double val;
+  try {
+    val = data.get<double>();
+    /*
+    std::cout << key << ":";
+    for (const auto& val : vec) {
+      std::cout << val << " ";
+    }
+    std::cout << std::endl;
+    */
+  } catch (std::exception err) {
+    std::ostringstream out;
+    out << "**ERROR**"
+        << " Variable \"" << key << "\" contains data in the wrong format in JSON file "
+        << filename << "\n"
+        << " Data expected to be of the form `key : val'.";
+    throw Uintah::ProblemSetupException(out.str(), __FILE__, __LINE__);
+  }
+  return val;
+}
+
+double 
+ElasticModuli_SupportVector::getVector1dJSON(const nlohmann::json& object, 
+                                             const std::string key,
+                                             const std::string& filename) const
+{
+  // First check if the key exists
+  nlohmann::json data;
+  try {
+    data = object.at(key);
+    // std::cout << "Data = " << data << std::endl;
+  } catch (std::out_of_range err) {
+    std::ostringstream out;
+    out << "**ERROR**"
+        << " Variable \"" << key << "\" not found in support vector regression JSON file "
+        << filename;
+    throw Uintah::ProblemSetupException(out.str(), __FILE__, __LINE__);
+  }
+
+  std::vector<double> vec;
+  try {
+    vec = data.get<std::vector<double>>();
+    /*
+    std::cout << key << ":";
+    for (const auto& val : vec) {
+      std::cout << val << " ";
+    }
+    std::cout << std::endl;
+    */
+  } catch (std::exception err) {
+    std::ostringstream out;
+    out << "**ERROR**"
+        << " Variable \"" << key << "\" contains data in the wrong format in JSON file "
+        << filename << "\n"
+        << " Data expected to be of the form `key : [val]'.";
+    throw Uintah::ProblemSetupException(out.str(), __FILE__, __LINE__);
+  }
+  return vec[0];
+}
+
+Eigen::Vector2d 
+ElasticModuli_SupportVector::getVector2dJSON(const nlohmann::json& object, 
+                                             const std::string key,
+                                             const std::string& filename) const
+{
+  // First check if the key exists
+  nlohmann::json data;
+  try {
+    data = object.at(key);
+    // std::cout << "Data = " << data << std::endl;
+  } catch (std::out_of_range err) {
+    std::ostringstream out;
+    out << "**ERROR**"
+        << " Variable \"" << key << "\" not found in support vector regression JSON file "
+        << filename;
+    throw Uintah::ProblemSetupException(out.str(), __FILE__, __LINE__);
+  }
+
+  std::vector<double> vec;
+  try {
+    vec = data.get<std::vector<double>>();
+    /*
+    std::cout << key << ":";
+    for (const auto& val : vec) {
+      std::cout << val << " ";
+    }
+    std::cout << std::endl;
+    */
+  } catch (std::exception err) {
+    std::ostringstream out;
+    out << "**ERROR**"
+        << " Variable \"" << key << "\" contains data in the wrong format in JSON file "
+        << filename << "\n"
+        << " Data expected to be of the form 'key : [x1, x2]'.";
+    throw Uintah::ProblemSetupException(out.str(), __FILE__, __LINE__);
+  }
+  return Eigen::Vector2d(vec.data());
+}
+
+Eigen::Matrix<double, Eigen::Dynamic, 1>
+ElasticModuli_SupportVector::getMatrixN1dJSON(const nlohmann::json& object,
+                                              const std::string key,
+                                              const std::string& filename) const
+{
+  // First check if the key exists
+  nlohmann::json data;
+  try {
+    data = object.at(key);
+    // std::cout << "Data = " << data << std::endl;
+  } catch (std::out_of_range err) {
+    std::ostringstream out;
+    out << "**ERROR**"
+        << " Variable \"" << key << "\" not found in support vector regression JSON file "
+        << filename;
+    throw Uintah::ProblemSetupException(out.str(), __FILE__, __LINE__);
+  }
+
+  std::vector<double> vec;
+  try {
+    vec = data.get<std::vector<double>>();
+    /*
+    std::cout << key << ":";
+    for (const auto& val : vec) {
+      std::cout << val << " ";
+    }
+    std::cout << std::endl;
+    */
+  } catch (std::exception err) {
+    std::ostringstream out;
+    out << "**ERROR**"
+        << " Variable \"" << key << "\" contains data in the wrong format in JSON file " 
+        << filename << "\n"
+        << " Data expected to be of the form 'key : [x1, x2, ..., x_n]^T'.";
+    throw Uintah::ProblemSetupException(out.str(), __FILE__, __LINE__);
+  }
+  return Eigen::Matrix<double, Eigen::Dynamic, 1>(vec.data());
+}
+
+Eigen::Matrix<double, Eigen::Dynamic, 2>
+ElasticModuli_SupportVector::getMatrixN2dJSON(const nlohmann::json& object,
+                                              const std::string key,
+                                              const std::string& filename) const
+{
+  // First check if the key exists
+  nlohmann::json data;
+  try {
+    data = object.at(key);
+    // std::cout << "Data = " << data << std::endl;
+  } catch (std::out_of_range err) {
+    std::ostringstream out;
+    out << "**ERROR**"
+        << " Variable \"" << key << "\" not found in support vector regression JSON file "
+        << filename;
+    throw Uintah::ProblemSetupException(out.str(), __FILE__, __LINE__);
+  }
+
+  std::vector<double> vec;
+  try {
+    vec = data.get<std::vector<double>>();
+    /*
+    std::cout << key << ":";
+    for (const auto& val : vec) {
+      std::cout << val << " ";
+    }
+    std::cout << std::endl;
+    */
+  } catch (std::exception err) {
+    std::ostringstream out;
+    out << "**ERROR**"
+        << " Variable \"" << key << "\" contains data in the wrong format in JSON file " 
+        << filename << "\n"
+        << " Data expected to be of the form 'key : [[x1, x2], [x3, x4], ..., [x_p, x_q]]^T'.";
+    throw Uintah::ProblemSetupException(out.str(), __FILE__, __LINE__);
+  }
+  return Eigen::Matrix<double, Eigen::Dynamic, 2>(vec.data());
+}
---
+++ Vaango/src/CCA/Components/MPM/ConstitutiveModel/ElasticModuliModels/ElasticModuli_SupportVector.h
@@ -0,0 +1,165 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2015-2020 Parresia Research Limited, New Zealand
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#ifndef ___ELASTIC_MODULI_SUPPORT_VECTOR_MODEL_H__
+#define ___ELASTIC_MODULI_SUPPORT_VECTOR_MODEL_H__
+
+#include <CCA/Components/MPM/ConstitutiveModel/ElasticModuliModels/ElasticModuliModel.h>
+#include <CCA/Components/MPM/ConstitutiveModel/ModelState/ModelStateBase.h>
+#include <Core/ProblemSpec/ProblemSpecP.h>
+
+#include <submodules/json/src/json.hpp>
+
+#include <limits>
+
+namespace Vaango {
+
+/** 
+ * ElasticModuli from a model fitted using support vector regression
+ */
+class ElasticModuli_SupportVector : public ElasticModuliModel
+{
+
+public:
+
+  ElasticModuli_SupportVector() = delete;
+  ElasticModuli_SupportVector(const ElasticModuli_SupportVector& smm) = delete;
+  ~ElasticModuli_SupportVector() = default;
+
+  ElasticModuli_SupportVector(Uintah::ProblemSpecP& ps);
+  ElasticModuli_SupportVector(const ElasticModuli_SupportVector* smm);
+  ElasticModuli_SupportVector& operator=(const ElasticModuli_SupportVector& smm) = delete;
+
+  void outputProblemSpec(Uintah::ProblemSpecP& ps) override;
+
+  /*! Get parameters */
+  std::map<std::string, double> getParameters() const override
+  {
+    std::map<std::string, double> params;
+    params["G0"] = d_shear.G0;
+    params["nu"] = d_shear.nu;
+    return params;
+  }
+
+  /*! Compute the elasticity */
+  ElasticModuli getInitialElasticModuli() const override;
+  ElasticModuli getCurrentElasticModuli(const ModelStateBase* state) const override;
+
+  ElasticModuli getElasticModuliLowerBound() const override
+  {
+    return getInitialElasticModuli();
+  }
+  ElasticModuli getElasticModuliUpperBound() const override
+  {
+    return ElasticModuli(std::numeric_limits<double>::max(),
+                         std::numeric_limits<double>::max());
+  }
+
+  /* Get the elastic moduli and their derivatives with respect to a single
+     plastic internal variable */
+  std::pair<ElasticModuli, ElasticModuli>
+    getElasticModuliAndDerivatives(const ModelStateBase* state_input) const override;
+
+  /*! Compute derivatives of moduli with respect to internal variables */
+  std::vector<ElasticModuli> computeDModuliDIntVar(const ModelStateBase* state) const override;
+
+  /*! Compute moduli and derivatives of moduli with respect to internal variables */
+  std::pair<ElasticModuli, std::vector<ElasticModuli>>
+  computeModuliAndDModuliDIntVar(const ModelStateBase* state) const override;
+
+private:
+
+  /* Tangent bulk modulus parameters */
+  struct BulkModulusParameters
+  {
+    std::string filename;
+    Eigen::Vector2d strain_conversion_factor;
+    Eigen::Vector2d strain_scale_factor;
+    Eigen::Vector2d strain_min;
+    Eigen::Vector2d strain_max;
+    double pressure_conversion_factor;
+    double pressure_scale_factor;
+    double pressure_min;
+    double pressure_max;
+    double rbf_kernel_gamma;
+    Eigen::Matrix<double, Eigen::Dynamic, 2> svr_support_vectors;
+    Eigen::Matrix<double, Eigen::Dynamic, 1> svr_dual_coeffs;
+    double svr_intercept;
+  };
+
+  /* Tangent shear modulus parameters */
+  struct ShearModulusParameters
+  {
+    double G0;
+    double nu;
+  };
+
+  BulkModulusParameters d_bulk;
+  ShearModulusParameters d_shear;
+
+  void checkInputParameters();
+
+  double computeBulkModulus(const double& elasticVolStrain,
+                            const double& plasticVolStrain) const;
+  double computeBulkModulusPressure(double pressure,
+                                    double plasticVolStrain) const;
+  double computeShearModulus(const double& bulkModulus) const;
+
+  /* Functions for handling JSON files */
+  void
+  readJSONFile(const std::string& filename);
+
+  nlohmann::json 
+  loadJSON(std::stringstream& inputStream,
+           const std::string& filename) const;
+
+  double 
+  getDoubleJSON(const nlohmann::json& object, 
+                const std::string key,
+                const std::string& filename) const;
+
+  double 
+  getVector1dJSON(const nlohmann::json& object, 
+                  const std::string key,
+                  const std::string& filename) const;
+
+  Eigen::Vector2d 
+  getVector2dJSON(const nlohmann::json& object, 
+                  const std::string key,
+                  const std::string& filename) const;
+
+  Eigen::Matrix<double, Eigen::Dynamic, 1>
+  getMatrixN1dJSON(const nlohmann::json& object,
+                   const std::string key,
+                   const std::string& filename) const;
+
+  Eigen::Matrix<double, Eigen::Dynamic, 2>
+  getMatrixN2dJSON(const nlohmann::json& object,
+                   const std::string key,
+                   const std::string& filename) const;
+
+};
+} // End namespace Vaango
+
+#endif // __ELASTIC_MODULI_SUPPORT_VECTOR_MODEL_H__
