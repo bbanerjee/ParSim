d5634a346ce1b7c90adbf7ce3eee5d694045869e
Biswajit Banerjee <banerjee@banerjee.banerjee.nz>
2022-08-12 14:40:05 +0530

    Updated compiler etc.

--- Vaango/src/StandAlone/restart_merger.cc
+++ Vaango/src/StandAlone/restart_merger.cc
@@ -3,6 +3,7 @@
  *
  * Copyright (c) 1997-2012 The University of Utah
  * Copyright (c) 2013-2014 Callaghan Innovation, New Zealand
+ * Copyright (c) 2014-2022 Parresia Research Limited, New Zealand
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
@@ -181,7 +182,7 @@ main( int argc, char *argv[], char *env[] )
     //Uintah::Parallel::finalizeManager(Uintah::Parallel::Abort);
     //abort();
     thrownException = true;
-  } catch (std::exception e){
+  } catch (std::exception& e){
     cerr << "Caught std exception: " << e.what() << '\n';
     //Uintah::Parallel::finalizeManager(Uintah::Parallel::Abort);
     //abort();
